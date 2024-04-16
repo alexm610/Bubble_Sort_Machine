da9a22959601ab14efa6e3bb5d134d7286169633
alexm610 <alexmanak@icloud.com>
2024-04-16 13:28:16 -0700

    added four-input multiplexer

--- source/reg_file.sv
+++ source/reg_file.sv
@@ -27,3 +27,20 @@ module multiplexer_two_input (A_in, B_in, select, C_out);
         endcase
     end
 endmodule: multiplexer_two_input
+
+module multiplexer_four_input (A_in, B_in, C_in, D_in, select, E_out);
+    parameter k = 32;
+    input logic [k-1:0] A_in, B_in, C_in, D_in;
+    input logic [1:0] select;
+    output logic [k-1:0] E_out;
+
+    always @(*) begin
+        case (select)
+            2'b00: E_out = A_in;
+            2'b01: E_out = B_in;
+            2'b10: E_out = C_in;
+            2'b11: E_out = D_in;
+            default: E_out = {k{1'b0}};
+        endcase
+    end
+endmodule: multiplexer_four_input
